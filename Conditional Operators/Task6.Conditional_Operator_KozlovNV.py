fac4430eabd2bed9f9bfc686760a214a513a544d
NiViKozlov <nivkozlov@mail.ru>
2023-12-03 12:04:42 +0300

    Task5

--- Conditional Operators/Task6.Conditional_Operator_KozlovNV.py
+++ Conditional Operators/Task6.Conditional_Operator_KozlovNV.py
@@ -1,2 +1,13 @@
 # Напишите программу, которая у пользователя запрашивает три числа  определяющие стороны треугольника. 
 # Определить тип треугольника:  равнобедренный, равносторонний, прямоугольный, обычный
+
+a = int(input('Enter first number":'))
+b = int(input('Enter second number:'))
+c = int(input('Enter third number'))
+
+if a != b != c and a != c != b:
+    print('Scalene triangle')
+elif a == b == c:
+    print('Equilateral triangle')
+else:
+    print('Isosceles triangle')
