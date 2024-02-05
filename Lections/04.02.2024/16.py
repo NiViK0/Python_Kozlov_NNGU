2d0240be9cc0b1f2223c0eed83cf74ac1612ca45
Никита Козлов <nikitakozlov@MacBook-Air-Nikita.local>
2024-02-05 16:22:03 +0300

    bibmbim

--- Lections/04.02.2024/16.py
+++ Lections/04.02.2024/16.py
@@ -5,3 +5,6 @@ sh_ip_int_br = [('FastEthernet0/0', '15.0.15.1', 'up', 'up'),
  ('Loopback0', '10.1.1.1', 'up', 'up'),
  ('Loopback100', '100.0.0.1', 'up', 'up')]
 print(tabulate(sh_ip_int_br))
+
+
+#доделать
